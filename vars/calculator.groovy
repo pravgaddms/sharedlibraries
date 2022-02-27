3ad5d6df35913500d9b3bc671eaa78dd30411854
pravgaddms <61373769+pravgaddms@users.noreply.github.com>
2022-02-27 16:31:54 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -1,7 +1,7 @@
 def add(x,y){
-  echo "Sum of ${x} and {y} is ${x+y}"
+  echo "Sum of ${x} and ${y} is ${x+y}"
 }
 def sub(x,y){
-  echo "sub of ${x} and {y} is ${x-y}"
+  echo "sub of ${x} and ${y} is ${x-y}"
 }
 
